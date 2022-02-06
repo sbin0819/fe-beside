7fa91de28c30f5299efa6457436a4acc0bfd255a
sbin0819 <sbinha123@gmail.com>
2022-02-06 18:38:25 +0900

    :sparkles: feat(ui): 회의 폼 수정

--- src/components/Meeting/MeetingForm.tsx
+++ src/components/Meeting/MeetingForm.tsx
@@ -1,19 +1,146 @@
 import React from 'react'
+import { Svg } from '@components/common'
+import { Notepad, noteViewBox } from '@svgs/Notepad'
+import { Pin, pinViewBox } from '@svgs/Pin'
+import { ActionItem, actionItemViewBox } from '@svgs/ActionItem'
+import { Add, addViewBox } from '@svgs/Add'
 import styled from 'styled-components'
 
 const Container = styled.div`
     margin-top: 20px;
-    border: 1px solid teal;
+    padding-right: 28px;
+    /* border: 1px solid teal; */
     height: 508px;
     overflow: scroll;
+    font-family: Pretendard;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: normal;
+    letter-spacing: normal;
+`
+
+const MenuContainer = styled.div<{ height?: number }>`
+    margin-top: 20px;
+    min-height: ${({ height }) => (height ? height : 120)}px;
+`
+
+const Header = styled.div`
+    display: flex;
+    margin-bottom: 14px;
+`
+
+const Body = styled.div`
+    width: 100%;
+    input {
+        width: 100%;
+    }
+`
+
+const ItemList = styled.div`
+    margin: 12px 0;
+`
+
+const Item = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    height: 103px;
+    padding: 16px 16px 17px 20px;
+    border: solid 1px #f1f1f1;
+    background-color: #fff;
+`
+
+const AddButton = styled.div`
+    display: flex;
+    align-items: center;
+    margin-top: 13px;
+    font-size: 12px;
+    font-weight: 500;
+    color: rgba(60, 60, 67, 0.6);
+    cursor: pointer;
 `
 
 function MeetingForm() {
     return (
         <Container>
-            <div>논의내용</div>
-            <div>결정된 사항</div>
-            <div>액션 아이템</div>
+            <MenuContainer height={150}>
+                <Header>
+                    <div>
+                        <Svg viewBox={noteViewBox} width={'20'} height={'18'}>
+                            <Notepad />
+                        </Svg>
+                    </div>
+                    <div>논의 내용</div>
+                </Header>
+                <Body>
+                    <input placeholder="논의할 내용에 대해 작성해주세요" />
+                </Body>
+            </MenuContainer>
+            <MenuContainer height={140}>
+                <Header>
+                    <div>
+                        <Svg viewBox={pinViewBox} width={'20'} height={'18'}>
+                            <Pin />
+                        </Svg>
+                    </div>
+                    <div>결정된 사항</div>
+                </Header>
+                <Body>
+                    <input placeholder="결정된 사항을 작성해주세요" />
+                </Body>
+            </MenuContainer>
+            <MenuContainer>
+                <Header>
+                    <div>
+                        <Svg
+                            viewBox={actionItemViewBox}
+                            width={'20'}
+                            height={'18'}
+                        >
+                            <ActionItem />
+                        </Svg>
+                    </div>
+                    <div>액션 아이템</div>
+                </Header>
+                <Body>
+                    <ItemList>
+                        <Item>
+                            <div>
+                                <input placeholder="액션 아이템을 작성해주세요" />
+                            </div>
+                            <div>
+                                <input placeholder="A 담당자" />
+                            </div>
+                            <div>
+                                <input placeholder="B 마감기한" />
+                            </div>
+                        </Item>
+                        <Item>
+                            <div>
+                                <input placeholder="액션 아이템을 작성해주세요" />
+                            </div>
+                            <div>
+                                <input placeholder="A 담당자" />
+                            </div>
+                            <div>
+                                <input placeholder="B 마감기한" />
+                            </div>
+                        </Item>
+                    </ItemList>
+                    <AddButton>
+                        <div>
+                            <Svg
+                                viewBox={addViewBox}
+                                width={'20'}
+                                height={'18'}
+                            >
+                                <Add />
+                            </Svg>
+                        </div>
+                        <div>액션 아이템 추가</div>
+                    </AddButton>
+                </Body>
+            </MenuContainer>
         </Container>
     )
 }
