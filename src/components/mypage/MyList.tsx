c8133fbdb898ecc044fe9ee8e3b57393c327103e
양다현 <govl1535@naver.com>
2022-02-11 02:00:29 +0900

    :sparkles: feat(ui): login page 이름 수정

--- src/components/mypage/MyList.tsx
+++ src/components/mypage/MyList.tsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import { Banner, Svg } from '@components/common'
 import { useRouter } from 'next/router'
+import useSWR from 'swr'
+import axios from 'axios'
 
 const TabContainer = styled.div`
     border: 1px solid blue;
@@ -10,9 +12,40 @@ const TabContainer = styled.div`
     border-radius: 24px;
     background-color: #fff;
 `
+const BoxContainer = styled.div`
+    border: 1px solid red;
+    width: 30%;
+    height: 30%;
+    margin: 10px;
+`
+const pMargin = {
+    margin: '5px',
+    border: '1px solid green',
+}
 
+const fetcher = (url) => axios.get(url).then((res) => res.data)
 function MyList() {
-    return <TabContainer>마이 리스트</TabContainer>
+    // const { data: meetDatas, error } = useSWR(
+    //     'http://127.0.0.1:8000/api/meet/',
+    //     fetcher,
+    //     { revalidateOnFocus: true }
+    // )
+
+    React.useEffect(() => {
+        // console.log('겟 미트 api', meetDatas)
+    }, [])
+    return (
+        <TabContainer>
+            {/* {meetDatas &&
+                meetDatas.map((meetData) => {
+                    ;<BoxContainer key={meetData.meet_id}>
+                        <p style={pMargin}>{meetData.status}</p>
+                        <p style={pMargin}>{meetData.meet_title}</p>
+                        <p style={pMargin}>{meetData.meet_date}</p>
+                    </BoxContainer>
+                })} */}
+        </TabContainer>
+    )
 }
 
 export default MyList
--- src/pages/login/landing.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Landing from '@components/Login/landing'
-
-function LandingPage() {
-    return <Landing />
-}
-
-export default LandingPage
