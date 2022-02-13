4415c2c506f18637a8d1d4456821f5d50b426129
양다현 <govl1535@naver.com>
2022-02-14 00:55:53 +0900

    :sparkles: feat(ui): 회의 리스트 정렬 api 연결

--- src/components/mypage/MyList.tsx
+++ src/components/mypage/MyList.tsx
@@ -4,6 +4,7 @@ import { Banner, Svg } from '@components/common'
 import { useRouter } from 'next/router'
 import useSWR from 'swr'
 import axios from 'axios'
+import moment from 'moment'
 
 const TabContainer = styled.div`
     border: 1px solid blue;
@@ -11,12 +12,57 @@ const TabContainer = styled.div`
     height: 500px;
     border-radius: 24px;
     background-color: #fff;
+    display: flex;
+    flex-direction: row;
+    // position: relative;
 `
 const BoxContainer = styled.div`
     border: 1px solid red;
     width: 30%;
     height: 30%;
     margin: 10px;
+    position: relative;
+    .status-name {
+        position: absolute;
+        right: 10px;
+        top: 10px;
+    }
+    .meet_title-name {
+        position: absolute;
+        left: 10px;
+        bottom: 50px;
+    }
+    .last_time-name {
+        position: absolute;
+        left: 10px;
+        bottom: 20px;
+    }
+`
+const DataNullBox = styled.div`
+    // background-color: yellow;
+    margin: 0 auto;
+    text-align: center;
+    color: 387878b;
+    font-size: 16px;
+    line-height: 1.5;
+    font-weight: 500;
+    .dataNullBtn {
+        // width: 170px;
+        height: 52px;
+        flex-grow: 0;
+        margin: 36px 97px 0;
+        padding: 14px 40px;
+        border-radius: 12px;
+        background-color: #0c254c;
+        font-size: 16px;
+        font-weight: 500;
+        font-stretch: normal;
+        font-style: normal;
+        line-height: 1.5;
+        letter-spacing: normal;
+        text-align: center;
+        color: #fff;
+    }
 `
 const pMargin = {
     margin: '5px',
@@ -25,25 +71,63 @@ const pMargin = {
 
 const fetcher = (url) => axios.get(url).then((res) => res.data)
 function MyList() {
-    // const { data: meetDatas, error } = useSWR(
-    //     'http://127.0.0.1:8000/api/meet/',
-    //     fetcher,
-    //     { revalidateOnFocus: true }
-    // )
-
+    const [testData, setTestData] = useState(undefined)
+    const { data: meetDatas, error } = useSWR(
+        'http://127.0.0.1:8000/api/meet/',
+        fetcher,
+        { revalidateOnFocus: true }
+    )
+    {
+        /* <div>
+                        <p>
+                            아직 회의록을 작성하지 않으셨나요? <br /> 팀원들과
+                            함께 효율적인 회의를 진행해보세요!
+                        </p>
+                        <button>회의 시작하기</button>
+                    </div> */
+    }
     React.useEffect(() => {
-        // console.log('겟 미트 api', meetDatas)
+        console.log('겟 미트 api', meetDatas)
     }, [])
     return (
         <TabContainer>
-            {/* {meetDatas &&
-                meetDatas.map((meetData) => {
-                    ;<BoxContainer key={meetData.meet_id}>
-                        <p style={pMargin}>{meetData.status}</p>
-                        <p style={pMargin}>{meetData.meet_title}</p>
-                        <p style={pMargin}>{meetData.meet_date}</p>
-                    </BoxContainer>
-                })} */}
+            {/* {meetDatas} */}
+            {meetDatas ? (
+                meetDatas.map((meetData: any) => {
+                    return (
+                        <BoxContainer key={meetData.meet_id}>
+                            <p style={pMargin} className="status-name">
+                                {meetData.status}
+                            </p>
+                            <p style={pMargin} className="meet_title-name">
+                                {meetData.meet_title}
+                            </p>
+                            <p style={pMargin} className="last_time-name">
+                                {moment(meetData.last_time).format(
+                                    'YYYY-MM-DD hh:mm'
+                                )}
+                            </p>
+                        </BoxContainer>
+                    )
+                })
+            ) : (
+                <DataNullBox>
+                    <div
+                        style={{
+                            width: '100px',
+                            height: '100px',
+                            backgroundColor: 'yellow',
+                        }}
+                    >
+                        데이터 없는 이미지
+                    </div>
+                    <p className="nullPstyle">
+                        아직 회의록을 작성하지 않으셨나요? <br /> 팀원들과 함께
+                        효율적인 회의를 진행해보세요!
+                    </p>
+                    <button className="dataNullBtn">회의 시작하기</button>
+                </DataNullBox>
+            )}
         </TabContainer>
     )
 }
