360a6d9d0daab50de24d472c0222a16565040834
sbin0819 <sbinha123@gmail.com>
2022-02-19 16:56:55 +0900

    :sparkles: feat(settings): meet/[id] 페이지 라우팅 id type change
    redux 연결
    patch api 요청 작업 debounce 고도화 작업이 필요해 보임

--- src/pages/meeting/[id].tsx
+++ src/pages/meeting/[id].tsx
@@ -17,7 +17,7 @@ export async function getStaticPaths() {
     const res = await fetch('http://125.6.40.68/api/meet/')
     const posts = await res.json()
     const paths = posts.map((post) => ({
-        params: { id: post.meet_id },
+        params: { id: '' + post.meet_id },
     }))
 
     return { paths, fallback: false }
@@ -25,8 +25,10 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params }) {
     const { id } = params
-    const resMeet = await fetch(`http://125.6.40.68/api/meet/${id}`)
-    const resAgenda = await fetch(`http://125.6.40.68/api/agenda?search=${id}`)
+    const resMeet = await fetch(`http://125.6.40.68/api/meet/${id.toString()}`)
+    const resAgenda = await fetch(
+        `http://125.6.40.68/api/agenda?search=${id.toString()}`
+    )
     const meet = await resMeet.json()
     const agendas = await resAgenda.json()
     return { props: { meet, agendas } }
