7f11c1662b19ca8db5467bddd0f843704194005a
양다현 <govl1535@naver.com>
2022-03-06 05:26:18 +0900

    :sparkles: feat(ui): 체크리스트 스타일, 점수구분, 미트연결

--- src/components/mypage/MyList.tsx
+++ src/components/mypage/MyList.tsx
@@ -11,7 +11,8 @@ import {
 import { Banner, Svg } from '@components/common'
 import { useRouter } from 'next/router'
 import useSWR from 'swr'
-import axios from 'axios'
+// import axios from 'axios'
+import axios from '@axios'
 import moment from 'moment'
 import NullDataList from './ListBox/NullDataList'
 import YDataList from './ListBox/YDataList'
@@ -19,41 +20,41 @@ import YDataList from './ListBox/YDataList'
 const fetcher = (url) => axios.get(url).then((res) => res.data)
 function MyList() {
     const [testData, setTestData] = useState(undefined)
-    // const { data, error } = useSWR(
-    //     'http://127.0.0.1:8000/api/meet/?search=Y',
-    //     fetcher,
-    //     { revalidateOnFocus: true }
-    // )
+    const { data, error } = useSWR(
+        'http://127.0.0.1:8000/api/meet/?meet_status=Y',
+        fetcher,
+        { revalidateOnFocus: true }
+    )
 
-    const data = [
-        {
-            user_id: 1,
-            meet_title: '제목입니다.',
-            meet_date: '2022-02-28T16:11:31.147Z',
-            meet_status: 'Y',
-            rm_status: 'W',
-            participants: 'user',
-            goal: 'goal',
-        },
-        {
-            user_id: 1,
-            meet_title: '제목입니다.2',
-            meet_date: '2022-02-28T16:11:31.147Z',
-            meet_status: 'P',
-            rm_status: 'W',
-            participants: 'user',
-            goal: 'goal',
-        },
-        {
-            user_id: 1,
-            meet_title: '제목입니다.3',
-            meet_date: '2022-02-28T16:11:31.147Z',
-            meet_status: 'C',
-            rm_status: 'N',
-            participants: 'user',
-            goal: 'goal',
-        },
-    ]
+    // const data = [
+    //     {
+    //         user_id: 1,
+    //         meet_title: '제목입니다.',
+    //         meet_date: '2022-02-28T16:11:31.147Z',
+    //         meet_status: 'Y',
+    //         rm_status: 'W',
+    //         participants: 'user',
+    //         goal: 'goal',
+    //     },
+    //     {
+    //         user_id: 1,
+    //         meet_title: '제목입니다.2',
+    //         meet_date: '2022-02-28T16:11:31.147Z',
+    //         meet_status: 'P',
+    //         rm_status: 'W',
+    //         participants: 'user',
+    //         goal: 'goal',
+    //     },
+    //     {
+    //         user_id: 1,
+    //         meet_title: '제목입니다.3',
+    //         meet_date: '2022-02-28T16:11:31.147Z',
+    //         meet_status: 'C',
+    //         rm_status: 'N',
+    //         participants: 'user',
+    //         goal: 'goal',
+    //     },
+    // ]
 
     React.useEffect(() => {
         console.log('겟 미트 api1', data)
