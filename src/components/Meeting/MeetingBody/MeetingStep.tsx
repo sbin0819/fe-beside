190cac0eaf9cdbcbea93c1d329f40956d0ea804a
sbin0819 <sbinha123@gmail.com>
2022-03-09 17:41:26 +0900

    :sparkles: feat(settings): step active 옵션 추가

--- src/components/Meeting/MeetingBody/MeetingStep.tsx
+++ src/components/Meeting/MeetingBody/MeetingStep.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { nanoid } from '@reduxjs/toolkit'
+import { current, nanoid } from '@reduxjs/toolkit'
 import styled from 'styled-components'
 import useMeeting from '@store/meeting/useMeeting'
 import useMeetingActions from '@store/meeting/useMeetingActions'
@@ -23,6 +23,10 @@ const MenulSteps = styled.div`
         color: #0c254c;
         cursor: pointer;
     }
+    .menu_step.curr_active {
+        background: #748298;
+        color: #fff;
+    }
     .menu_step.active {
         background: #384c6c;
         color: #fff;
@@ -37,13 +41,23 @@ function MeetingStep() {
     const { agendas, agendaCursor } = useMeeting()
     const { setAgendaCursor } = useMeetingActions()
 
+    const realActive =
+        agendas.findIndex((el) => el.agenda_status == 'p') === -1
+            ? agendas.length - 1
+            : agendas.findIndex((el) => el.agenda_status == 'p')
+
     return (
         <MenulSteps>
             {agendas.map((data, idx) => (
                 <div
                     className={`menu_step ${
-                        agendaCursor === idx ? 'active' : ''
-                    } ${data.agenda_status === 'y' && 'disabled'}`}
+                        agendaCursor === idx && realActive ? 'curr_active' : ''
+                    } ${
+                        agendaCursor === realActive && agendaCursor === idx
+                            ? 'active'
+                            : ''
+                    }
+                    ${data.agenda_status === 'y' && 'disabled'}`}
                     key={data.agenda_id}
                     onClick={() => {
                         if (data.agenda_status !== 'y') {
