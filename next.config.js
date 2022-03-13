ad1aa307702b12433f5c589d755079da21c277ba
sbin0819 <sbinha123@gmail.com>
2022-03-13 20:45:08 +0900

    feat: config 수정

--- next.config.js
+++ next.config.js
@@ -3,13 +3,13 @@
  */
 const nextConfig = {
     /* config options here */
-    webpackDevMiddleware: (config) => {
-        config.watchOptions = {
-            poll: 1000,
-            aggregateTimeout: 300,
-        }
-        return config
-    },
+    // webpackDevMiddleware: (config) => {
+    //     config.watchOptions = {
+    //         poll: 1000,
+    //         aggregateTimeout: 300,
+    //     }
+    //     return config
+    // },
     env: {
         APP_ENV: String(process.env.APP_ENV),
     },
