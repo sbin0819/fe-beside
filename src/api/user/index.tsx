e2574805edc92602d41743286bbda75768f7702b
양다현 <govl1535@naver.com>
2022-03-23 03:12:30 +0900

    유저 이모지 체크

--- src/api/user/index.tsx
+++ src/api/user/index.tsx
@@ -5,14 +5,22 @@ import { Cookies } from 'react-cookie'
 const cookies = new Cookies()
 
 export const userSWR = () => {
-    const { data: userData, mutate: userMutate } = useSWR(
-        `${baseURL}/api/user/`,
-        (url) =>
-            fetch(url, {
-                headers: {
-                    Authorization: cookies.get('Authorization'),
-                },
-            }).then((res) => res.json())
+    const { data: userData, mutate: userMutate } = useSWR(`${baseURL}/api/user/`, (url) =>
+        fetch(url, {
+            headers: {
+                Authorization: cookies.get('Authorization'),
+            },
+        }).then((res) => res.json())
     )
     return { userData, userMutate }
+}
+export const emojiSWR = (id) => {
+    const { data: emojiData, mutate: emojiMutate } = useSWR(id ? `${baseURL}/api/emoji/${id}/` : null, (url) =>
+        fetch(url, {
+            headers: {
+                Authorization: cookies.get('Authorization'),
+            },
+        }).then((res) => res.json())
+    )
+    return { emojiData, emojiMutate }
 }
